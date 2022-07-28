3f72f62f1600268254d799b079d5c365170e5b8d
roma69rus <roma69rus@gmail.com>
2022-07-28 14:22:07 +0300

    123

---
+++ src/components/UI/loader/MyLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './MyLoader.module.css'
+
+const MyLoader = () => {
+  return ( 
+    <div className={s.loader}>
+      
+    </div>
+   );
+}
+ 
+export default MyLoader;
